86beb55b1dc70972d84fd142ed25a314449c5754
AcidicSoil <claytonbivens1@gmail.com>
2024-05-29 10:37:44 -0500

    Initial push of script

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,7 @@
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
--- src/services/services.js
+++ src/services/services.js
@@ -1,9 +1,31 @@
-import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/services'
+import axios from 'axios';
+const baseUrl = '/api/services';
+
+let token = null;
+
+const setToken = newToken => {
+  token = `Bearer ${newToken}`;
+};
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+  const request = axios.get(baseUrl);
+  return request.then(response => response.data);
+};
 
-export default { getAll, create, update }
+const create = newObject => {
+  const config = {
+    headers: { Authorization: token },
+  };
+
+  const request = axios.post(baseUrl, newObject, config);
+  return request.then(response => response.data);
+};
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  return request.then(response => response.data);
+};
+
+export default {
+  getAll, create, update, setToken
+};
